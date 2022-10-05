910a2053352b49d2a099e2964708031fb167da2c
AndresKempke <kempke@tesolva.de>
2022-10-05 10:27:20 +0200

    added some first routes to the buttons in homepage * once in next page---fix background

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import { Routes, Route } from 'react-router-dom';
 import { Background } from './components';
 
 import { SelectGame, Score, History, Home } from './pages/index';
+import GlobalStyle from './globalstyle/globalStyle';
 
 export default function App() {
   return (
-    <div>
-      <Background />
-      <Routes>
-        <Route path={ROUTES.Home} element={<Home />} />
-        <Route path={ROUTES.Score} element={<Score />} />
-        <Route path={ROUTES.SelectGame} element={<SelectGame />} />
-        <Route path={ROUTES.History} element={<History />} />
-      </Routes>
-    </div>
+    <>
+      <GlobalStyle>
+        <Background />
+        <div style={{ display: 'flex' }}>
+          <Routes>
+            <Route path={ROUTES.Home} element={<Home />} />
+            <Route path={ROUTES.Score} element={<Score />} />
+            <Route path={ROUTES.SelectGame} element={<SelectGame />} />
+            <Route path={ROUTES.History} element={<History />} />
+          </Routes>
+        </div>
+      </GlobalStyle>
+    </>
   );
 }
