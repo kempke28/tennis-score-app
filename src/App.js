a2d7224ea44ae423745296efbdd6943159513907
Andres Kempke <79771436+kempke28@users.noreply.github.com>
2022-10-05 10:44:36 +0200

    Create routes in buttons (#17)
    * added some first routes to the buttons in homepage
    * once in next page fix background
    
    * added some first routes to the buttons in homepage
    * once in next page---fix background
    
    * Update src/App.js
    
    Thomas fragement suggestion :)
    
    Co-authored-by: Thomas Kroth <52001609+krug33@users.noreply.github.com>
    
    Co-authored-by: AndresKempke <kempke@tesolva.de>
    Co-authored-by: Thomas Kroth <52001609+krug33@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import { Routes, Route } from 'react-router-dom';
 import { Background } from './components';
 
 import { SelectGame, Score, History, Home } from './pages/index';
+import GlobalStyle from './globalstyle/globalStyle';
 
 export default function App() {
   return (
-    <div>
+    <GlobalStyle>
       <Background />
-      <Routes>
-        <Route path={ROUTES.Home} element={<Home />} />
-        <Route path={ROUTES.Score} element={<Score />} />
-        <Route path={ROUTES.SelectGame} element={<SelectGame />} />
-        <Route path={ROUTES.History} element={<History />} />
-      </Routes>
-    </div>
+      <div style={{ display: 'flex' }}>
+        <Routes>
+          <Route path={ROUTES.Home} element={<Home />} />
+          <Route path={ROUTES.Score} element={<Score />} />
+          <Route path={ROUTES.SelectGame} element={<SelectGame />} />
+          <Route path={ROUTES.History} element={<History />} />
+        </Routes>
+      </div>
+    </GlobalStyle>
   );
 }
---
+++ src/globalstyle/globalStyle.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './globalStyle.css';
+
+export default function GlobalStyle({ children }) {
+  return <div>{children}</div>;
+}
