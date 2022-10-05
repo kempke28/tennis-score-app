e5d547e88da8286a86fe4e322a810511547f88bb
Andres Kempke <79771436+kempke28@users.noreply.github.com>
2022-10-05 10:39:41 +0200

    Update src/App.js
    Thomas fragement suggestion :)
    
    Co-authored-by: Thomas Kroth <52001609+krug33@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -8,18 +8,16 @@ import GlobalStyle from './globalstyle/globalStyle';
 
 export default function App() {
   return (
-    <>
-      <GlobalStyle>
-        <Background />
-        <div style={{ display: 'flex' }}>
-          <Routes>
-            <Route path={ROUTES.Home} element={<Home />} />
-            <Route path={ROUTES.Score} element={<Score />} />
-            <Route path={ROUTES.SelectGame} element={<SelectGame />} />
-            <Route path={ROUTES.History} element={<History />} />
-          </Routes>
-        </div>
-      </GlobalStyle>
-    </>
+    <GlobalStyle>
+      <Background />
+      <div style={{ display: 'flex' }}>
+        <Routes>
+          <Route path={ROUTES.Home} element={<Home />} />
+          <Route path={ROUTES.Score} element={<Score />} />
+          <Route path={ROUTES.SelectGame} element={<SelectGame />} />
+          <Route path={ROUTES.History} element={<History />} />
+        </Routes>
+      </div>
+    </GlobalStyle>
   );
 }
