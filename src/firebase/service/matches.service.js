43ea961b6422793d7f8c393153eb819beb6256f1
AndresKempke <kempke@tesolva.de>
2023-02-09 12:12:36 +0100

    updated firebase functions

--- src/firebase/service/matches.service.js
+++ src/firebase/service/matches.service.js
@@ -1,14 +1,13 @@
-import { collection, getDocs, doc, setDoc } from 'firebase/firestore/lite';
+import { collection, getDocs, doc, setDoc, getDoc } from 'firebase/firestore/lite';
 import { db } from '../config/firebase-config';
-import * as ROUTES from '../../routes/routes';
 
 // Saves a new collection to Cloud Firestore.
 export async function saveMatches(matchSettings) {
   const time = new Date();
   const matchRef = collection(db, 'match');
-  const emptyMatchRef = doc(matchRef);
+  const newMatchRef = doc(matchRef);
   try {
-    await setDoc(emptyMatchRef, {
+    await setDoc(newMatchRef, {
       matchType: matchSettings.matchType,
       player1: matchSettings.player1, //input
       player2: matchSettings.player2, //input
@@ -23,10 +22,29 @@ export async function saveMatches(matchSettings) {
   } catch (error) {
     alert('data could not be saved');
 
-    return false
+    return null;
     //or any other message
   }
-  return true
+  return newMatchRef.id;
+}
+
+export async function createSetsCollection(matchId) {
+  const SetCollection = collection(db, 'sets');
+  const newSetCollection = doc(SetCollection);
+  await setDoc(newSetCollection, {
+    matchId: matchId,
+    setNumber: 0,
+    details: {
+      player1: {
+        score: 0,
+        games: []
+      },
+      player2: {
+        score: 0,
+        games: []
+      }
+    }
+  });
 }
 
 // Get a list of matches from your database
@@ -36,4 +54,12 @@ export async function getMatches() {
   const matchSnapshot = await getDocs(matchReq);
   const matchList = matchSnapshot.docs.map((match) => match.data());
   return matchList;
+}
+
+export async function getMatchById(matchId) {
+  const matchRef = collection(db, 'match');
+  const matchDocRef = doc(matchRef, matchId);
+  const matchSnapshot = await getDoc(matchDocRef);
+  const matchDoc = { ...matchSnapshot.data(), id: matchSnapshot.id };
+  return matchDoc;
 }
