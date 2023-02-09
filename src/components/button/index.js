0512bde42308dab1ff1ffccbb2cbfbce570c89c3
AndresKempke <kempke@tesolva.de>
2023-02-09 11:20:16 +0100

    updated components

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { StyledButton, SettingsButton } from './style/button';
+import { StyledButton, SettingsButton, ScoringButton } from './style/button';
 
-export default function Button({children, ...props }) {
-  return (
-
-    <StyledButton {...props}>
-      {children}
-    </StyledButton>
-  );
+export default function Button({ children, ...props }) {
+    return <StyledButton {...props}>{children}</StyledButton>;
 }
 
 Button.SetttingsButton = function SetButton({ children, ...Props }) {
-  return <SettingsButton {...Props}>{children}</SettingsButton>;
+    return <SettingsButton {...Props}>{children}</SettingsButton>;
 };
 
+Button.ScoringButton = function ScoreButton({ children, ...props }) {
+    return <ScoringButton {...props}>{children}</ScoringButton>;
+};
 
